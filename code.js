1254b20067948c0c25bbeb1908890237f9bf31f8
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-01-26 16:21:34 -0700

    Update code.js
    added code for reverse insertion sort

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-function insertionSortReverse(array) {
-    return array;
+function insertionSortReverse(arr) {
+    for (var i = arr.length-2; i >= 0; i--) {
+        // Invariant: the elements in arr[i..arr.length-1] are in sorted order
+        var val = arr[i];
+        var j;
+        console.log(arr)
+        for (j = i; j < arr.length && arr[j+1] < val; j++) {
+            arr[j] = arr[j+1];
+        }
+        arr[j] = val;
+    }
+    return arr;
 }
